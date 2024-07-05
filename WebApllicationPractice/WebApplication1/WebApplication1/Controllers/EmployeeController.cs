c4def320323dbc64c0cb70be27384046a21e5231
b106user06 <b106user06@rpsconsulting.in>
2024-07-05 18:47:41 +0530

    practice

---
+++ WebApllicationPractice/WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeFormApp.Models;
+using System;
+using System.Collections.Generic; // Include this for List<T>
+
+namespace EmployeeFormApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static List<EmployeeForm> _employees = new List<EmployeeForm>(); // Example in-memory list
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Example: Save data to a list (in-memory) or database
+                _employees.Add(model);
+
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(_employees);
+        }
+    }
+}
